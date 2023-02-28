d9997d228e019f62941ea25455e72140b7d56c49
Andrey Kharginen <kut13@inbox.ru>
2023-02-28 11:46:12 +0400

    change code

--- src/test/java/ProductManagerTest.java
+++ src/test/java/ProductManagerTest.java
@@ -44,6 +44,19 @@ public class ProductManagerTest {
         Product[] expected = {book1, book2};
         Assertions.assertArrayEquals(expected, actual);
     }
-
+    @Test
+    public void searchBy0(){
+        ProductRepository repo = new ProductRepository();
+        ProductManager manager = new ProductManager(repo);
+        Book book1 = new Book(1, "Моя весна", 200, "Александр Шубкин");
+        Book book2 = new Book(2, "Моя осень", 400, "Николай Бубкин");
+        Smartphone smartphone1 = new Smartphone(3, "аsa", 10000, "samsung");
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(smartphone1);
+        Product[] actual = manager.searchBy("Твоя весна ");
+        Product[] expected = {};
+        Assertions.assertArrayEquals(expected, actual);
+    }
 
 }
